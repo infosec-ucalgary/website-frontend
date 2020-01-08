1b951498271a29856fb2328701ea4b0afa5a3d8c
Joshua Novak <jdjnovak@gmail.com>
2020-01-08 14:35:45 -0700

    Browser didn't like the comments, so moved them below the imports

--- app/main.js
+++ app/main.js
@@ -1,20 +1,19 @@
-// Import routing
 import router from './routing/index.js';
 import Route from './routing/Route.js';
 
-// Import templates/views
 import homeView from './templates/home.js';
 import aboutView from './templates/about.js';
-//import loginView from './templates/login.js'; // Not for now
-//import registerView from './templates/register.js'; // Not for now
 import contactView from './templates/contact-us.js';
 import resourcesView from './templates/resources.js';
 
-// Create routes for each page
+// Add other view imports here
+
 var homeRoute = new Route('home', '/', homeView);
 var aboutRoute = new Route('about', '/about', aboutView);
 var homeRoute = new Route('contact', '/resources', contactView);
 var homeRoute = new Route('resources', '/resources', resourcesView);
+
+// Add other routes here
 
 const routes = [ homeRoute, aboutRoute, contactRoute, resourcesRoute ];
 
