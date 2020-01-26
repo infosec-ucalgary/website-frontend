c8aed7ce13bdcbc94bb42e163149caab30214d41
Joshua Novak <jdjnovak@gmail.com>
2020-01-25 21:17:40 -0700

    Added a new "Not Found/404" component with view and routing

---
+++ src/components/NotFoundComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="_text-center">
+    <h1>404 - Not Found</h1>
+    <p>The page you're looking for does not exist.</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'NotFoundComponent'
+}
+</script>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <NotFoundComponent />
+</template>
+<script>
+import NotFoundComponent from '@/components/NotFoundComponent.vue'
+
+export default {
+  name: 'notfound',
+  components: {
+    NotFoundComponent
+  }
+}
+</script>
