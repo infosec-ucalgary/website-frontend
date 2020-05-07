2108745110992f3aaa34800c51ccf156b77c5f46
Joshua Novak <jdjnovak@gmail.com>
2020-05-06 20:42:57 -0600

    Started to move over to TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './public/**/*.html',
+      './src/**/*.vue'
+    ]
+  },
+  theme: {
+    extend: {},
+    fontFamily: {
+      'display': ['Lato'],
+    },
+  },
+  variants: {},
+  plugins: [],
+}
