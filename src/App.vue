f7178482a048f3417cc8377fe217dfe076a11d57
Joshua Novak <jdjnovak@gmail.com>
2020-05-30 12:33:52 -0600

    Moved navbar to component. Made it properly repsonsive.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <!-- <div class="_position-relative _width-100" id="outer-container"> -->
   <div class="relative w-full" id="outer-container">
-    <div class="bg-bar w-auto h-16">
-      <div class="relative w-2/3 m-auto p-4">
-        <router-link to="/"><span class="pr-8 text-white font-display text-xl inline-block align-middle">UofC InfoSec Club</span></router-link>
-        <router-link class="pr-8 text-gray-300 font-display text-md inline-block align-middle" to="/">Home</router-link>
-        <router-link class="pr-8 text-gray-300 font-display text-md inline-block align-middle" to="/calendar">Calendar</router-link>
-        <router-link class="pr-8 text-gray-300 font-display text-md inline-block align-middle" to="/resources">Resources</router-link>
+
+    <!-- NAVBAR -->
+    <NavbarComponent />
+
+    <!-- ALERT -->
+    <div class="text-center py-4 lg:px-4" id="upcoming-alert">
+      <div class="p-2 bg-accent-2 items-center leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+        <span class="flex rounded-full bg-bar text-white uppercase px-2 py-1 text-xs font-display mr-3">New</span>
+        <span class="font-display mr-2 text-left flex-auto" style="font-color: #12418f;">The exec team is working hard to bring you an exciting fall and winter semester! Stay tuned!</span>
+        <svg class="fill-current opacity-75 h-4 w-4" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" v-on:click="hideAlert()">
+          <title>Close</title>
+          <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+        </svg>
       </div>
     </div>
 
-    <!--
-    <i-alert dismissible :show="visible" variant="primary" class="_text-center">
-      <p><i class="inkline-icon -info"></i> Having trouble with a challenge or want to know more about a topic we discussed at a meeting? Come see us every friday from 12 PM to 2PM for our office hours in MS 119!</p>
-    </i-alert>
-    -->
-
+    <!-- PAGE RENDER -->
     <router-view />
 
+    <!-- FOOTER -->
     <div id="footer" class="_text-center">
       <div class="_height-100 _display-inline-block _align-middle"><p>We are a group of students from University of Calgary who are dedicated to learn cyber security - &copy;2020</p></div>
     </div>
@@ -44,10 +47,22 @@
 </style>
 
 <script>
+import NavbarComponent from './components/NavbarComponent.vue'
+
 export default {
+  name: 'app',
+  components: {
+    NavbarComponent
+  },
   data () {
     return {
       visible: true
+    }
+  },
+  methods: {
+    hideAlert: function () {
+      let alert = document.getElementById('upcoming-alert')
+      if (alert.style.display !== 'none') alert.style.display = 'none'
     }
   }
 }
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="flex items-center justify-center flex-wrap bg-bar">
+    <div class="w-5/6 lg:w-2/3 flex items-center justify-between flex-wrap p-2">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <img class="fill-current h-12 w-12 mr-2" width="54" height="54" src="@/assets/logo.svg" alt="SVG Logo">
+        <span class="font-display text-xl tracking-tight">UofC InfoSec Club</span>
+      </div>
+      <div class="block lg:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-gray-300 border-gray-500 hover:text-white hover:border-white">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" v-on:click="isOpen = !isOpen">
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div :class="isOpen ? 'block' : 'hidden'" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <router-link id="nav-item" class="block mt-4 lg:inline-block lg:mt-0 text-gray-400 hover:text-white font-display text-lg mr-4" to="/">Home</router-link>
+          <router-link id="nav-item" class="block mt-4 lg:inline-block lg:mt-0 text-gray-400 hover:text-white font-display text-lg mr-4" to="/calendar">Calendar</router-link>
+          <router-link id="nav-item" class="block mt-4 lg:inline-block lg:mt-0 text-gray-400 hover:text-white font-display text-lg " to="/resources">Resources</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
