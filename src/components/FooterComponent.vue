e8283145498c5551ad65965cf019facabe40e1b0
Joshua Novak <jdjnovak@gmail.com>
2020-08-27 03:35:24 -0600

    Moved mailchimp above the copyright

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="mt-16">
-    <div id="" class="pb-6 w-full text-center inline-block align-middle">
-      <span>We are a group of students from the University of Calgary who are dedicated to learning cyber security - &copy;2020</span>
-    </div>
     <div class="mx-auto mb-4 text-center">
       <MailchimpComponent />
+    </div>
+    <div id="" class="pb-6 w-full text-center inline-block align-middle">
+      <span>We are a group of students from the University of Calgary who are dedicated to learning cyber security - &copy;2020</span>
     </div>
   </div>
 </template>
