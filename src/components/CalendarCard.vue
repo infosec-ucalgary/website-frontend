3f6529c04fe9eb18c2ba5fc16eea73e7c9474619
James Lowther <jamesevlowther@gmail.com>
2020-08-26 19:41:52 -0600

    Changed calendar card height value

--- src/components/CalendarCard.vue
+++ src/components/CalendarCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mx-auto my-4 py-4 flex-shrink-0 border border-gray-300 shadow rounded-lg w-48 h-48 lg:m-4">
+  <section class="mx-auto my-4 py-4 flex-shrink-0 border border-gray-300 shadow rounded-lg w-48 h-auto lg:m-4">
     <div class="font-display font-medium text-lg mb-2">{{ date }}</div>
     <div class="font-display font-bold">{{ title }}</div>
     <p>{{ desc }}</p>
