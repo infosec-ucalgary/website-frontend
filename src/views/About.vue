5a26f6e0c4c35967559325cae5472da532ff28ee
James Lowther <jamesevlowther@gmail.com>
2020-09-04 19:46:29 -0600

    Added Ejaaz's headshot

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,7 +13,7 @@
         <BioCard name="Joshua Novak" title="VP of Tech" path="https://infosecucalgary.ca/uploads/josh-headshot.jpg" secret="Webmaster Supreme" />
         <BioCard name="James Lowther" title="Junior Executive" path="https://infosecucalgary.ca/uploads/james-headshot.jpg" secret="Crunchwrap Supreme" />
         <BioCard name="Aarti Barakale" title="Junior Executive" path="https://infosecucalgary.ca/uploads/aarti-headshot.jpg" />
-        <BioCard name="Ejaaz Lakhani" title="Junior Executive" />
+        <BioCard name="Ejaaz Lakhani" title="Junior Executive" path="https://infosecucalgary.ca/uploads/ejaaz-headshot.jpg" />
       </div>
     </div>
   </section>
