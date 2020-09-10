4ed5f084c1c3388f978304081c1018b926acb525
James Lowther <jamesevlowther@gmail.com>
2020-09-10 16:50:07 -0600

    Added speaker series skeleton page

---
+++ src/views/Speakers.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="py-8 md:w-1/2 mx-auto text-center">
+    <p class="font-display text-4xl">Speaker Series</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Speakers'
+}
+</script>
