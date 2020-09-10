c49038adc2d9f4ace79c51748a169e7da23cdb5b
James Lowther <jamesevlowther@gmail.com>
2020-09-10 16:50:07 -0600

    Added speaker series skeleton page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Calendar from '../views/Calendar.vue'
+import Speakers from '../views/Speakers.vue'
 import Resources from '../views/Resources.vue'
 import NotFound from '../views/NotFound.vue'
 
@@ -23,6 +24,11 @@ const routes = [
     path: '/calendar',
     name: 'calendar',
     component: Calendar
+  },
+  {
+    path: '/speakers',
+    name: 'speakers',
+    component: Speakers
   },
   {
     path: '/resources',
