9f72c5381a6bd33c09470463c5d4686a9c0f04b3
Joshua Novak <jdjnovak@gmail.com>
2020-10-27 19:27:40 -0600

    Removed unused imports and Vue.use() commands

--- src/main.js
+++ src/main.js
@@ -1,27 +1,15 @@
 import Vue from 'vue'
 import './assets/css/output.css'
 import '@inkline/inkline/dist/inkline.css'
-import Inkline from '@inkline/inkline'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import VModal from 'vue-js-modal'
 
-import Calendar from 'primevue/calendar'
-import DataTable from 'primevue/datatable'
-import Column from 'primevue/column'
-import ColumnGroup from 'primevue/columngroup'
-
 Vue.config.productionTip = false
 
-Vue.use(Inkline)
 Vue.use(VModal)
-
-Vue.component('Calendar', Calendar)
-Vue.component('DataTable', DataTable)
-Vue.component('Column', Column)
-Vue.component('ColumnGroup', ColumnGroup)
 
 new Vue({
   router,
