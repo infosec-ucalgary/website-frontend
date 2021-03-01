ecbce6e837d6fdfd1fe0d806d8f69282d07d34a2
Joshua Novak <jdjnovak@gmail.com>
2021-03-01 15:16:24 -0700

    Fix for CSS size issues

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import './assets/css/output.css'
 import App from './App.vue'
+import './index.css'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
