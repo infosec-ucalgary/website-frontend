e9c6fe87137c7419ff7fdcfd67a630c6557bc1a8
Daniel Sabourov <59862537+SteamBlizzard@users.noreply.github.com>
2022-08-25 19:03:21 -0600

    Update About.vue
    Added new execs.

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,6 +8,7 @@
         <BioCard name="Greg Vance" title="VP Finance" path="https://infosecucalgary.ca/uploads/greg-headshot.jpg" secret="Something Something Supreme"/>
         <BioCard name="Maryam Bouh" title="VP Operations" path="https://infosecucalgary.ca/uploads/maryam-headshot.jpg"  />
         <BioCard name="Marianna Ferreira" title="VP Communications" path="https://infosecucalgary.ca/uploads/marianna-headshot.jpg"  />
+        <BioCard name="Waleed Ghumman" title="VP Secretary" path="https://infosecucalgary.ca/uploads/marianna-headshot.jpg"  />
         <BioCard name="Delara Shamanian" title="VP Tech" path="https://infosecucalgary.ca/uploads/delara-headshot.jpg" secret="DevOps Supreme" />
         <BioCard name="Ethan Sengsavang" title="VP Tech" path="https://infosecucalgary.ca/uploads/ethan-headshot.jpg" secret="just supremely tired" />
         <BioCard name="Ned Liu" title="VP Tech" path="https://infosecucalgary.ca/uploads/ned-headshot.jpg"  />
@@ -16,13 +17,20 @@
       <div class="inline-block lg:mx-0 lg:flex lg:flex-wrap mx-auto w-auto justify-center">
         <BioCard name="Amna Adnan" path="https://infosecucalgary.ca/uploads/amna-headshot.jpg" />
         <BioCard name="Brandon Arenas" path="https://infosecucalgary.ca/uploads/not-brandon-but-brandon.png" />
-        <BioCard name="Christopher Schultze" path="https://infosecucalgary.ca/uploads/chris-headshot.jpg"  />
         <BioCard name="Ejaaz Lakhani" path="https://infosecucalgary.ca/uploads/ejaaz-headshot.jpg" secret="Supreme Beard" />
         <BioCard name="Pratham Pandey" path="https://infosecucalgary.ca/uploads/pratham-headshot.jpg" />
-        <BioCard name="Zachary Gilchrist" path="https://infosecucalgary.ca/uploads/zachary-headshot.jpg" />
       </div>
     <!-- div class="font-display text-3xl my-4" style="text-decoration:underline">Junior Executives</div>
       <div class="inline-block lg:mx-0 lg:flex lg:flex-wrap mx-auto w-auto justify-center">
+        <BioCard name="Amna Shiekh" path="https://infosecucalgary.ca/uploads/amna-s-headshot.jpg" />
+        <BioCard name="Artemy Gavrilov" path="https://infosecucalgary.ca/uploads/artemy-headshot.jpg" />
+        <BioCard name="Auric Poku" path="https://infosecucalgary.ca/uploads/auric-headshot.jpg" />
+        <BioCard name="Christina He" path="https://infosecucalgary.ca/uploads/christina-headshot.jpg" />
+        <BioCard name="Dwayne Liang" path="https://infosecucalgary.ca/uploads/dwayne-headshot.jpg" />
+        <BioCard name="Divyansh Rana" path="https://infosecucalgary.ca/uploads/divyansh-headshot.jpg" />
+        <BioCard name="Fam Ghaly" path="https://infosecucalgary.ca/uploads/fam-headshot.jpg" />
+        <BioCard name="Julie Kim" path="https://infosecucalgary.ca/uploads/julie-headshot.jpg" />
+        <BioCard name="Kimih Yan" path="https://infosecucalgary.ca/uploads/kimih-headshot.jpg" />
       </div>
     <div class="font-display text-4xl my-4"></div -->
   </section>
