55ca75ca6f908690deadec7bfd127dab28cbcc82
Nicholasm24 <nicholasmackinnon9@gmail.com>
2024-06-28 14:41:53 -0600

    grid voodoo magic to make biocards more balanced
    - adjusted tailwind a bit to make biocards into cleaner 3x2 grid instead of uneven rows
    - added div to BioCard that centers contents. Added breakpoints that are probably useless but I'm too lazy to figure out if it effects the biocards when not in a grid.
    - fixed up formatting cuz prettier isn't working, I might minecraft myself
    xoxo

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,14 @@
   <section class="text-center md:w-2/3 mx-auto">
     <div class="font-display text-4xl my-4">About Us </div>
     <div class="font-display text-3xl my-4" style="text-decoration:underline">Executives</div>
-      <div class="inline-block lg:mx-0 lg:flex lg:flex-wrap mx-auto w-auto justify-center">
-        <BioCard name="Fam Ghaly" title="President" path="https://infosecucalgary.ca/uploads/fam-headshot.jpg" />
-        <BioCard name="Sean Robertson" title="Secretary" path="https://infosecucalgary.ca/uploads/sean-headshot.jpg" />
-        <BioCard name="Jason Ngu" title="VP Communications" path="https://infosecucalgary.ca/uploads/jason-headshot.jpg" />
-        <BioCard name="Nicholas MacKinnon" title="VP Tech" path="https://infosecucalgary.ca/uploads/nick-headshot.jpg" />
-        <BioCard name="Royce K" title="VP Tech" path="https://infosecucalgary.ca/uploads/royce-headshot.png"  />
-        <BioCard name="Dante Kirsman" title="VP Tech" path="https://infosecucalgary.ca/uploads/dante-headshot.jpg" />
-      </div>
+    <div class="inline-block xl:grid xl:grid-cols-3 lg:mx-0 lg:flex lg:flex-wrap mx-auto w-auto justify-center">
+      <BioCard name="Fam Ghaly" title="President" path="https://infosecucalgary.ca/uploads/fam-headshot.jpg" />
+      <BioCard name="Sean Robertson" title="Secretary" path="https://infosecucalgary.ca/uploads/sean-headshot.jpg" />
+      <BioCard name="Jason Ngu" title="VP Communications" path="https://infosecucalgary.ca/uploads/jason-headshot.jpg" />
+      <BioCard name="Nicholas MacKinnon" title="VP Tech" path="https://infosecucalgary.ca/uploads/nick-headshot.jpg" />
+      <BioCard name="Royce K" title="VP Tech" path="https://infosecucalgary.ca/uploads/royce-headshot.png"  />
+      <BioCard name="Dante Kirsman" title="VP Tech" path="https://infosecucalgary.ca/uploads/dante-headshot.jpg" />
+    </div>
     <div class="font-display text-3xl my-4" style="text-decoration:underline">Directors</div>
       <div class="inline-block lg:mx-0 lg:flex lg:flex-wrap mx-auto w-auto justify-center">
         <BioCard name="Christina He" path="https://infosecucalgary.ca/uploads/christina-headshot.jpg" />
